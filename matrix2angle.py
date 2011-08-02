cbc1a447bd4e19036c4f397d686cc7a59a211df1
phelrine <phelrine@gmail.com>
2011-08-02 23:19:13 +0900

    内包表記

--- matrix2angle.py
+++ matrix2angle.py
@@ -5,54 +5,48 @@ from math import pi
 from scipy import sqrt, arctan2, arcsin, arccos, sin, cos, dot
 
 def flatten(L):
-    if isinstance(L, list):
-        if L == []:
-            return []
-        else:
-            return flatten(L[0]) + flatten(L[1:])
-    else:
-        return [L]
+    if isinstance(L, list): return [] if L == [] else flatten(L[0]) + flatten(L[1:])
+    else: return [L]
 
 def main():
-    COLUMNS = { "HEAD": 6, "NECK": 20, "TORSO": 34,
-                "LEFT_SHOULDER": 48, "LEFT_ELBOW": 62,
-                "RIGHT_SHOULDER": 90, "RIGHT_ELBOW": 104 }
-    AXES = ["X", "Y", "Z"]
-    data = []
-
-    header = []
-    for key in sorted(COLUMNS.keys()):
-        header.extend(map(lambda a: key + "_" + a, AXES))
-    data.append(header)
-    
+    columns = ["HEAD", "NECK", "TORSO", "LEFT_SHOULDER", "LEFT_ELBOW", "RIGHT_SHOULDER", "RIGHT_ELBOW"]
     reader = csv.reader(sys.stdin)
-    reader.next()
-    for line in reader:
-        matrixes = dict(map((lambda(joint, column): 
-                             (joint, scipy.array(map((lambda x: float(x)), line[column : (column + 9)])).reshape(3, 3))),
-                            COLUMNS.items()))
-        data.append(flatten(map(lambda(key, val): convert_matrix_to_angle(val), sorted(convert_world_to_local(matrixes).items()))))
+    header = parse_header(reader.next())
 
-    fix_jump(data)    
+    data = []
+    data.append(flatten([[column + "_" + label for label in ["X", "Y", "Z"]] for column in columns]))
+    for row in reader:
+        matrixes = dict([(column, scipy.array([float(x) for x in row[header[column]:(header[column]+9)]]).reshape(3,3)) for column in columns])
+        converted = convert_absolute_to_relative(matrixes)
+        data.append(flatten([matrix_to_angle(converted[column]) for column in columns]))
+
+    fix_jump(data)
+
     csv.writer(sys.stdout).writerows(data)
 
-def convert_world_to_local(joint_matrix):
-    converted_matrix = {}
-    converted_matrix["TORSO"] = joint_matrix["TORSO"]
-    invTorso = joint_matrix["TORSO"].transpose()
-    converted_matrix["NECK"] = dot(invTorso, joint_matrix["NECK"])
-    invNeck = converted_matrix["NECK"].transpose()
-    converted_matrix["HEAD"] = dot(invNeck, joint_matrix["HEAD"])
-    converted_matrix["LEFT_SHOULDER"] = dot(invNeck, joint_matrix["LEFT_SHOULDER"])
-    converted_matrix["LEFT_ELBOW"] = dot(converted_matrix["LEFT_SHOULDER"], joint_matrix["LEFT_ELBOW"])
-    converted_matrix["RIGHT_SHOULDER"] = dot(invNeck, joint_matrix["RIGHT_SHOULDER"])
-    converted_matrix["RIGHT_ELBOW"] = dot(converted_matrix["RIGHT_SHOULDER"], joint_matrix["RIGHT_ELBOW"])
-    return converted_matrix
+def parse_header(header):
+    columns = {}
+    for i in range(len(header)): columns[header[i]] = i + 5
+    del columns['']
+    return columns
 
-def convert_matrix_to_angle(m):
+def convert_absolute_to_relative(matrixes):
+    converted = {}
+    converted["TORSO"] = matrixes["TORSO"]
+    invTorso = matrixes["TORSO"].transpose()
+    converted["NECK"] = dot(invTorso, matrixes["NECK"])
+    invNeck = converted["NECK"].transpose()
+    converted["HEAD"] = dot(invNeck, matrixes["HEAD"])
+    converted["LEFT_SHOULDER"] = dot(invNeck, matrixes["LEFT_SHOULDER"])
+    converted["LEFT_ELBOW"] = dot(converted["LEFT_SHOULDER"], matrixes["LEFT_ELBOW"])
+    converted["RIGHT_SHOULDER"] = dot(invNeck, matrixes["RIGHT_SHOULDER"])
+    converted["RIGHT_ELBOW"] = dot(converted["RIGHT_SHOULDER"], matrixes["RIGHT_ELBOW"])
+    return converted
+
+def matrix_to_angle(m):
     y = arcsin(-m[2, 0])
-    x = arctan2(m[2,1], m[2,2])
-    z = arctan2(m[1,0], m[0,0])
+    x = arctan2(m[2, 1], m[2, 2])
+    z = arctan2(m[1, 0], m[0, 0])
     return [x, y, z]
 
 def fix_jump(data):
